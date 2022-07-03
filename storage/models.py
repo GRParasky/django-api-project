f56c4bef727c056fd6d94950cfd2a3514b53fa0b
GRParasky <grparasky@gmail.com>
2022-07-03 16:16:24 -0300

    Creating Storage app and creating Product class

---
+++ storage/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from owner.models import Owner
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(Owner,
+                              on_delete=models.CASCADE)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    price = models.FloatField()
