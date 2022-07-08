b9407aaf83b6e6d2942bac94f269060a990ddfce
GRParasky <grparasky@gmail.com>
2022-07-08 18:38:37 -0300

    Setting Owner columns as non-editable

--- owner/models.py
+++ owner/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Owner(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(editable=False,
+                            max_length=255)
