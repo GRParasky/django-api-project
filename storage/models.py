8806059ffb8e83b343af21c744a5042976253c76
GRParasky <grparasky@gmail.com>
2022-07-08 18:38:13 -0300

    Setting Product columns as non-editable

--- storage/models.py
+++ storage/models.py
@@ -5,8 +5,10 @@ from owner.models import Owner
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(Owner,
+                              editable=False,
                               on_delete=models.CASCADE)
-    created_datetime = models.DateTimeField(auto_now_add=True)
+    created_datetime = models.DateTimeField(auto_now_add=True,
+                                            editable=False)
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
     price = models.FloatField()
