bfb4504ca8dde3a7c2d1ac5944f8b8ca63871c19
GRParasky <grparasky@gmail.com>
2022-07-08 18:42:48 -0300

    Creating URL filters to allow query-params

--- storage/api/viewsets.py
+++ storage/api/viewsets.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets
 
 from storage.api import serializers
-from storage import models
+from storage.models import Product
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
-    queryset = models.Product.objects.all()
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        title = self.request.query_params.get('title')
+        price = self.request.query_params.get('price')
+
+        if title is not None:
+            queryset = queryset.filter(title=title)
+        elif price is not None:
+            queryset = queryset.filter(price=price)
+        elif title is not None and price is not None:
+            queryset = queryset.filter(title=title, price=price)
+
+        return queryset
