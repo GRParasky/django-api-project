6dc8d99f2db39c8cda2f61ccbfe6999418ed569f
GRParasky <grparasky@gmail.com>
2022-07-10 09:45:37 -0300

    Setting Heroku configs to deploy

--- store/settings.py
+++ store/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
+
+
+# Configure Django App for Heroku.
+django_on_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
